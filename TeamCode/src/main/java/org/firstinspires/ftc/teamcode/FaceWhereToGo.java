d4904c7fee59c67ef753caf9ed89b671375182f7
Cody Graham <codydgraham@gmail.com>
2018-11-02 15:56:48 -0400

    Final test.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FaceWhereToGo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FaceWhereToGo.java
@@ -7,9 +7,7 @@ public class FaceWhereToGo extends Teleop_Parent {
 
     //Team is using Regular/H drive train
     Heading heading;
-    boolean isTurning = false;
-    boolean dpadPressed = false;
-    boolean isStopped = false;
+    boolean resetPid = true;
 
     @Override
     public void setup() {
@@ -26,25 +24,51 @@ public class FaceWhereToGo extends Teleop_Parent {
     @Override
     public void repeat() {
         // Get powers
-        double away = -gamepad1.left_stick_y;
-        double side = gamepad1.left_stick_x;
-        double strafe = gamepad1.right_stick_x;
+        double awayDirectional = -gamepad1.left_stick_y;
+        double sideDirectional = gamepad1.left_stick_x;
+        double awayStraight = -gamepad1.right_stick_y;
+        double sideStraight = gamepad1.right_stick_x;
 
-        if (Math.abs(away) < JOYSTICK_DEAD_ZONE && Math.abs(side) < JOYSTICK_DEAD_ZONE
-                && Math.abs(strafe) < JOYSTICK_DEAD_ZONE) {
+        boolean directional = Math.abs(awayDirectional) > JOYSTICK_DEAD_ZONE || Math.abs(sideDirectional) > JOYSTICK_DEAD_ZONE;
+        boolean straight = Math.abs(awayStraight) > JOYSTICK_DEAD_ZONE || Math.abs(sideStraight) > JOYSTICK_DEAD_ZONE;
+
+        if (!directional && !straight) {
             setDrive(0.00, 0.00, 0.00);
             holdTurnPID.update(heading.getRelativeHeading());
-        } else {
+            resetPid = true;
+        } else if (directional) {
+            double directionOfJoystick = Math.toDegrees(Math.atan2(sideDirectional,awayDirectional));
+            double inputSize = Math.sqrt(Math.pow(awayDirectional, 2) + Math.pow(sideDirectional, 2));
 
-            double directionOfJoystick = Math.toDegrees(Math.atan2(side,away));
-            double inputSize = Math.sqrt(Math.pow(away, 2) + Math.pow(side, 2));
+            heading.setRelativeOffset((float)-directionOfJoystick);
+            double pidTurnPower = holdTurnPID.update(heading.getRelativeHeading());
 
-            // If user is trying to drive around
-            if (Math.abs(away) > JOYSTICK_DEAD_ZONE || Math.abs(away) > JOYSTICK_DEAD_ZONE)
-                heading.setRelativeOffset((float)-directionOfJoystick);
+            setDrive(inputSize, pidTurnPower, 0.0);
+
+            telemetry.addData("pgain", p);
+            telemetry.addData("dgain", d);
+            telemetry.addData("Joystick Direction", directionOfJoystick);
+            telemetry.addData("Joystick Power", inputSize);
+            telemetry.update();
+            resetPid = true;
+        } else { // Straight
+            double directionOfJoystick = Math.toDegrees(Math.atan2(sideStraight,awayStraight));
+            double inputSize = Math.sqrt(Math.pow(sideDirectional, 2) + Math.pow(sideStraight, 2));
+
+            double fieldHeading = Heading.getFieldHeading();
+            double robotOrientedCommand = directionOfJoystick - fieldHeading;
+            double strafe = inputSize * Math.sin(Math.toRadians(robotOrientedCommand));
+            double forward = inputSize * Math.cos(Math.toRadians(robotOrientedCommand));
+
+            if (resetPid)
+            {
+                heading.setRelativeOffset((float)-fieldHeading);
+                resetPid = false;
+            }
 
             double pidTurnPower = holdTurnPID.update(heading.getRelativeHeading());
-            setDrive(inputSize, pidTurnPower, strafe);
+
+            setDrive(forward, pidTurnPower, strafe);
 
             telemetry.addData("pgain", p);
             telemetry.addData("dgain", d);
