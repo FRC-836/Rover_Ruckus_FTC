73c9eac4027bf5f4a35450e7aaf9c81be0511358
FLL_4 <FTC.12888@gmail.com>
2018-11-03 15:11:40 -0400

    Refined PID gains

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm_PID_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arm_PID_Test.java
@@ -8,7 +8,10 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @Autonomous (name = "Arm PID Testing", group = "A")
 public class Arm_PID_Test extends LinearOpMode{
     private DcMotor armMotor;
-    protected PID_Controller moveArmPid = new PID_Controller(0.00042,0.00028,0.0 );
+
+    private boolean repeated = false;
+
+    protected PID_Controller moveArmPid = new PID_Controller(0.003,0.00028,0.00042 );
     @Override
     public void runOpMode() throws InterruptedException {
         armMotor = hardwareMap.get(DcMotor.class, "arm");
@@ -16,21 +19,31 @@ public class Arm_PID_Test extends LinearOpMode{
         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
         armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         waitForStart();
 
         moveArmPid.setSetpoint(armMotor.getCurrentPosition() + 1800);
+        moveArmPid.resetPID();
+
+        int resetPoint = armMotor.getCurrentPosition() + 1100;
+
         while(opModeIsActive()) {
             armMotor.setPower(moveArmPid.update(armMotor.getCurrentPosition()));
+            if (armMotor.getCurrentPosition() >= resetPoint && !repeated) {
+                repeated = true;
+                moveArmPid.resetPID();
+            }
             moveArmPid.displayCurrentPID(telemetry);
             telemetry.addData("Goal: ", moveArmPid.getSetpoint());
             telemetry.addData("Arm Encoder",armMotor.getCurrentPosition());
+            telemetry.addData("Int stopped", repeated);
             telemetry.update();
         }
     }
 }
 
 /* moving 1800 encoder counts (positive)
-p : 0.00042
+p : 0.003
 i : 0.00028
-d :
+d : 0.00042
  */
