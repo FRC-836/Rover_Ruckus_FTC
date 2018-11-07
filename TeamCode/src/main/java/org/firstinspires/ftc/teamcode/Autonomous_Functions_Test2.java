8bc404dc0ae38690272233ae1a5f077ffd991b57
Surface Book <ftc.00389@gmail.com>
2018-11-06 19:35:35 -0500

    Updated and added autonomous functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Functions_Test2.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Autonomous Functions Test 2", group = "A")
+public class Autonomous_Functions_Test2 extends Autonomous_Parent {
+
+
+
+
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Parent.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import static com.sun.tools.doclint.Entity.and;
+import static com.sun.tools.doclint.Entity.mu;
 import static com.sun.tools.doclint.Entity.or;
 import static com.sun.tools.doclint.Entity.thorn;
 
@@ -12,6 +13,7 @@ public abstract class Autonomous_Parent extends Robot_Parent {
 
     private final double EC_PER_IN = 104.7;
     private final double SECONDS_PER_IN = 0.16;
+    private final double SECONDS_PER_DEGREE = 0.03;
     private final long CYCLE_TIME_MS = 5;
 
     @Override
@@ -40,59 +42,88 @@ public abstract class Autonomous_Parent extends Robot_Parent {
         return position;
     }
 
-    protected void driveForwardPID(double inches) {
+    protected void driveDistancePID(double inches) {
+        Heading retain = Heading.createRelativeHeading(0.0f);
         forwardPID.setSetpoint(getForwardPosition() + inches);
-        holdTurnPID.setSetpoint(getTurnPosition());
+        holdTurnPID.setSetpoint(0.0);
+        forwardPID.resetPID();
+        holdTurnPID.resetPID();
         int iterations = 0;
         int numIterations = (int) (SECONDS_PER_IN * 1000.0 * inches / CYCLE_TIME_MS);
         while (opModeIsActive() && (iterations <= numIterations)) {
-            setDrive(forwardPID.update(getForwardPosition()), holdTurnPID.update(getTurnPosition()), 0.0);
-            iterations++;
-            sleep(CYCLE_TIME_MS);
-        }
-        setDrive(0.0, 0.0, 0.0);
-    }
-    protected void driveBackwardPID(double inches) {
-        forwardPID.setSetpoint(getForwardPosition() - inches);
-        holdTurnPID.setSetpoint(getTurnPosition());
-        int iterations = 0;
-        int numIterations = (int) (SECONDS_PER_IN * 1000.0 * inches / CYCLE_TIME_MS);
-        while (opModeIsActive() && (iterations <= numIterations)) {
-            setDrive(forwardPID.update(getForwardPosition()), holdTurnPID.update(getTurnPosition()), 0.0);
+            setDrive(forwardPID.update(getForwardPosition()), holdTurnPID.update(retain.getRelativeHeading()), 0.0);
             iterations++;
             sleep(CYCLE_TIME_MS);
         }
         setDrive(0.0, 0.0, 0.0);
     }
 
-    protected void driveForwardNorm(double inches) {
-        double finalDistance = getForwardPosition() + inches;
-        while (getForwardPosition() < finalDistance && opModeIsActive()) {
-            setDrive(1.0, 0.0, 0.0);
+    protected void driveStrafePID(double inches) {
+        Heading retain = Heading.createRelativeHeading(0.0f);
+        strafePID.setSetpoint(getStrafePosition() + inches);
+        holdTurnPID.setSetpoint(0.0);
+        strafePID.resetPID();
+        holdTurnPID.resetPID();
+        int iterations = 0;
+        int numIterations = (int) (SECONDS_PER_IN * 1000.0 * inches / CYCLE_TIME_MS);
+        while (opModeIsActive() && (iterations <= numIterations)) {
+            setDrive(0.0, holdTurnPID.update(retain.getRelativeHeading()), strafePID.update(getStrafePosition()));
+            iterations++;
+            sleep(CYCLE_TIME_MS);
         }
         setDrive(0.0, 0.0, 0.0);
     }
 
-    protected void driveTurnRightNorm(double degrees) {
-        double finalDistance = getTurnPosition() + degrees;
-        while (getForwardPosition() < finalDistance && opModeIsActive()) {
-            setDrive(0.0, 1.0, 0.0);
+    protected void driveTurnPID(double degrees) {
+        Heading goal = Heading.createRelativeHeading((float) degrees);
+        turnPID.setSetpoint(0.0);
+        turnPID.resetPID();
+        int iterations = 0;
+        int numIterations = (int) (SECONDS_PER_DEGREE * 1000.0 * degrees / CYCLE_TIME_MS);
+        while (opModeIsActive() && (iterations <= numIterations)) {
+            setDrive(0.0, turnPID.update(goal.getRelativeOffset()), 0.0);
+            iterations++;
+            sleep(CYCLE_TIME_MS);
         }
         setDrive(0.0, 0.0, 0.0);
     }
 
-    protected void driveBackwardNorm(double inches) {
-        double finalDistance = getForwardPosition() - inches;
-        while (getForwardPosition() > finalDistance && opModeIsActive()) {
-            setDrive(-1.0, 0.0, 0.0);
+    protected void driveDistance(double inches) {
+        Heading retain = Heading.createRelativeHeading(0.0f);
+        holdTurnPID.setSetpoint(0.0);
+        holdTurnPID.resetPID();
+        double finalPosition = getForwardPosition() + inches;
+        double multiplier = 1.0;
+        if (inches < 0.0)
+            multiplier = -1.0;
+        while (getForwardPosition() * multiplier < finalPosition * multiplier && opModeIsActive()) {
+            setDrive(multiplier, holdTurnPID.update(retain.getRelativeOffset()), 0.0);
         }
         setDrive(0.0, 0.0, 0.0);
     }
 
-    protected void driveTurnLeftNorm(double degrees) {
-        double finalDistance = getTurnPosition() - degrees;
-        while (getForwardPosition() > finalDistance && opModeIsActive()) {
-            setDrive(0.0, -1.0, 0.0);
+    protected void driveStrafe(double inches) {
+        Heading retain = Heading.createRelativeHeading(0.0f);
+        holdTurnPID.setSetpoint(0.0);
+        holdTurnPID.resetPID();
+        double finalPosition = getStrafePosition() + inches;
+        double multiplier = 1.0;
+        if (inches < 0.0)
+            multiplier = -1.0;
+        while (getStrafePosition() * multiplier < finalPosition * multiplier && opModeIsActive()) {
+            setDrive(0.0, holdTurnPID.update(retain.getRelativeOffset()), multiplier);
         }
+        setDrive(0.0, 0.0, 0.0);
+    }
+
+    protected void driveTurn(double degrees) {
+        Heading goal = Heading.createRelativeHeading((float)-degrees);
+        double multiplier = 1.0;
+        if (degrees < 0.0)
+            multiplier = -1.0;
+        while (goal.getRelativeHeading() * multiplier < 0.0 && opModeIsActive()) {
+            setDrive(0.0, multiplier, 0.0);
+        }
+        setDrive(0.0, 0.0, 0.0);
     }
 }
