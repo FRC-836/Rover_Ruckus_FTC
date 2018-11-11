b504817a6bbdbaaf716fde507a8a5161f2f04747
Andrew Woodworth <manlydrew2@gmail.com>
2018-11-11 13:03:45 -0500

    fixed heading/TargetDirection setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot_Parent.java
@@ -38,8 +38,7 @@ public abstract class Robot_Parent extends LinearOpMode {
 
         setupImu();
 
-        Heading.setImu(imu);
-        Heading.setFieldOffset(-Heading.getAbsoluteHeading());
+        TargetDirection.setupImu(0.0, imu);
 
         initialize();
         telemetry.addLine("Initialization Complete");
