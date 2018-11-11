c4cc44313bf39792a51c9e59c935fd2db35147e8
Tyler Gunther <tylgmd@gmail.com>
2018-11-11 14:35:57 -0600

    Updated branch from master and dev

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arcade_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Arcade_Drive.java
@@ -7,8 +7,6 @@ public class Arcade_Drive extends Teleop_Parent {
 
     //Team is using Regular/H drive train
 
-    Heading heading;
-
     @Override
     public void setup() {
 
@@ -17,8 +15,6 @@ public class Arcade_Drive extends Teleop_Parent {
     @Override
     public void begin() {
 
-        heading = Heading.createRelativeHeading(0.0f);
-
     }
 
     @Override
@@ -26,7 +22,7 @@ public class Arcade_Drive extends Teleop_Parent {
         // Get powers
         double forwardPower = -gamepad1.left_stick_y;
         double turnPower = gamepad1.right_stick_x;
-        double pidTurnPower = holdTurnPID.update(heading.getRelativeHeading());
+        
         // If user is trying to turn
         if (Math.abs(turnPower) > JOYSTICK_DEAD_ZONE) {
             setDrive(forwardPower, turnPower);
