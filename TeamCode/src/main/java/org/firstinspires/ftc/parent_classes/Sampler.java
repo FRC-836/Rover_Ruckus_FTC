d5ce12b815b512c35919513b98f52767d12bf6f1
Andrew Woodworth <manlydrew2@gmail.com>
2018-11-24 15:57:12 -0500

    setup different sampling options for Sampler

--- TeamCode/src/main/java/org/firstinspires/ftc/parent_classes/Sampler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/parent_classes/Sampler.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
+import java.util.Vector;
 
 public class Sampler {
     private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
@@ -19,6 +20,9 @@ public class Sampler {
     private static final String VUFORIA_KEY = "AVOMBtn/////AAAAGWoe5Xr26kwMi197rJ9ukz9CynZI0gtPtGmyzJJF/DX5fVRX6KZpGATlLq8PX8m1UBpwePrOZAkZmI/XhtlNcZ3fbgvlGuYfDwkaUgbaJgrzPut88uA5KdcubkI0uTw/J+S2y/jGLeKM3pnXMxywmGjbNNGRLgt57I9pThzUjuwC8jNi42C3Qo17qpTiHOFRWbRybxGDKk2PAUEVdCFtjW4zuzB+b5xBABGCytzwdPk+riWGBSITihc1tPIWtOP1CDQiMp4B3V1ysJTPxOTXkWmFuSqqFhgePWKoXxVqbcWVcpUDQ1yVPdpK9BxrZfeoVrpWGRfBa9mrSYsB6VBKkKIxMClow9gxYv5SyN440Btn";
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
+
+    private final float LEFT_OF_SCREEN = 640.0f;
+    private final float RIGHT_OF_SCREEN = 1280.0f;
 
     public enum GoldPosition {
         LEFT,
@@ -53,33 +57,107 @@ public class Sampler {
             List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
             if (updatedRecognitions != null) {
                 // Code to react to updated recognitions goes below this line!
-                if (updatedRecognitions.size() == 3) {
-                    int goldMineralY = -1;
-                    int silverMineral1Y = -1;
-                    int silverMineral2Y = -1;
-                    for (Recognition recognition : updatedRecognitions) {
-                        if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
-                            goldMineralY = (int) recognition.getBottom();
-                        } else if (silverMineral1Y == -1) {
-                            silverMineral1Y = (int) recognition.getBottom();
-                        } else {
-                            silverMineral2Y = (int) recognition.getBottom();
-                        }
+                Vector<Recognition> silverList = new Vector<>();
+                Recognition bestGold = null;
+                for (Recognition newRecognition : updatedRecognitions) {
+                    if (newRecognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                        if (bestGold == null)
+                            bestGold = newRecognition;
+                        else
+                            bestGold = findBestGold(newRecognition, bestGold);
+                    } else if (newRecognition.getLabel().equals(LABEL_SILVER_MINERAL)){
+                        silverList.add(newRecognition);
                     }
-                    if (goldMineralY != -1 && silverMineral1Y != -1 && silverMineral2Y != -1) {
-                        if (goldMineralY < silverMineral1Y && goldMineralY < silverMineral2Y) {
-                            return GoldPosition.LEFT;
-                        } else if (goldMineralY > silverMineral1Y && goldMineralY > silverMineral2Y) {
-                            return GoldPosition.RIGHT;
-                        } else {
+                }
+                if (silverList.size() > 2)
+                    silverList = findBestTwoSilver(silverList);
+
+                //mineral left half of screen (810 pixels)
+                //mineral right far right of screen
+                if (bestGold == null)
+                {
+                    if (silverList.size() == 2)
+                    {
+                        float silverPos1 = silverList.elementAt(0).getBottom();
+                        float silverPos2 = silverList.elementAt(1).getBottom();
+                        if ((silverPos1 < LEFT_OF_SCREEN && silverPos2 < LEFT_OF_SCREEN) ||
+                                (silverPos1 > RIGHT_OF_SCREEN && silverPos2 > RIGHT_OF_SCREEN) ||
+                                (silverPos1 >= LEFT_OF_SCREEN && silverPos2 >= LEFT_OF_SCREEN &&
+                                silverPos1 <= RIGHT_OF_SCREEN && silverPos2 <= RIGHT_OF_SCREEN))
+                        {
+                            silverList.remove(1);
+                        }
+                        else
+                        {
+                            boolean silverLeft = silverPos1 < LEFT_OF_SCREEN || silverPos2 < LEFT_OF_SCREEN;
+                            boolean silverRight = silverPos1 > RIGHT_OF_SCREEN || silverPos2 > RIGHT_OF_SCREEN;
+                            if (!silverLeft)
+                                return GoldPosition.LEFT;
+                            if (!silverRight)
+                                return GoldPosition.RIGHT;
                             return GoldPosition.CENTER;
                         }
+                    }
+                    if (silverList.size() == 1)
+                    {
+                        float silverPos = silverList.elementAt(0).getBottom();
+                        if (silverPos <= LEFT_OF_SCREEN)
+                            return GoldPosition.CENTER;
+                        else if (silverPos < RIGHT_OF_SCREEN)
+                            return GoldPosition.RIGHT;
+                        else
+                            return GoldPosition.CENTER;
+                    }
+                    if (silverList.size() == 0)
+                    {
+                        return GoldPosition.UNKNOWN;
+                    }
+                }
+                else
+                {
+                    switch (silverList.size())
+                    {
+                        case 0:
+                        case 1:
+                        case 2:
+                            // Use the gold position alone to determine location
+                            if (bestGold.getBottom() > RIGHT_OF_SCREEN)
+                                return GoldPosition.RIGHT;
+                            else if (bestGold.getBottom() < LEFT_OF_SCREEN)
+                                return GoldPosition.LEFT;
+                            else
+                                return GoldPosition.CENTER;
+                            /*
+                            // Determine location of gold based on location relative to 2 silvers
+                            if (bestGold.getBottom() > silverList.elementAt(0).getBottom() &&
+                                    bestGold.getBottom() > silverList.elementAt(1).getBottom())
+                                return GoldPosition.LEFT;
+                            else if (bestGold.getBottom() < silverList.elementAt(0).getBottom() &&
+                                    bestGold.getBottom() < silverList.elementAt(1).getBottom())
+                                return GoldPosition.RIGHT;
+                            else
+                                return GoldPosition.CENTER;
+                                */
                     }
                 }
                 // Code to react to updated recognitions goes above this line!
             }
         }
         return GoldPosition.UNKNOWN;
+    }
+
+    private Vector<Recognition> findBestTwoSilver(Vector<Recognition> silverList) {
+        // TODO: Write actual comparison
+        while (silverList.size() > 2)
+            silverList.remove(2);
+        return silverList;
+    }
+
+    private Recognition findBestGold(Recognition rec1, Recognition rec2) {
+        if (rec1.getConfidence() > rec2.getConfidence())
+            return rec1;
+        else
+            return rec2;
     }
 
     public void shutdown() {
