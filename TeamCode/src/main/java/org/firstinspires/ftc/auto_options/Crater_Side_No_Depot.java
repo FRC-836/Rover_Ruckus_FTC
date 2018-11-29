bb4a4bc0d084fd2e27c56e228c49e719323bce3e
Surface Book <ftc.00389@gmail.com>
2018-11-29 18:43:00 -0500

    fixed name of Crater_Side_No_Depot

--- TeamCode/src/main/java/org/firstinspires/ftc/auto_options/Crater_Side_No_Depot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/auto_options/Crater_Side_No_Depot.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.parent_classes.Autonomous_Parent;
 
-@Autonomous(name = "Crater_Side")
+@Autonomous(name = "Crater_Side_No_Depot")
 public class Crater_Side_No_Depot extends Autonomous_Parent {
     @Override
     public void setup() {
