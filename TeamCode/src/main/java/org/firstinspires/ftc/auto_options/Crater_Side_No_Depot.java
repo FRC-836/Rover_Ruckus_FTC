29dce172288bb4ae8c013206481e796201fe1e48
Surface Book <ftc.00389@gmail.com>
2018-11-29 18:41:09 -0500

    created another auto program for crater side and not traveling to depot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/auto_options/Crater_Side_No_Depot.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.auto_options;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.parent_classes.Autonomous_Parent;
+
+@Autonomous(name = "Crater_Side")
+public class Crater_Side_No_Depot extends Autonomous_Parent {
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void begin() {
+        land();
+        sampleParkCrater();
+    }
+}
