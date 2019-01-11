eb4afd6c78928911430b58a084a37aab0ca8ec9d
Andrew Woodworth <manlydrew2@gmail.com>
2019-01-10 20:05:31 -0500

    modified teloep arcade program

--- TeamCode/src/main/java/org/firstinspires/ftc/teleop_options/Arcade_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teleop_options/Arcade_Drive.java
@@ -49,11 +49,11 @@ public class Arcade_Drive extends Teleop_Parent {
         setLandingMotorPower(latchingPower);
 
         if (gamepad1.right_bumper)
-            setIntakeLifter(1.0);
+            setIntakeLifter(0.8);
         else if (gamepad1.right_trigger > 0.5)
-            setIntakeLifter(-1.0);
+            setIntakeLifter(-0.7);
         else
-            setIntakeLifter(0.05);
+            setIntakeLifter(0.15);
 
         if (gamepad1.b)
             setMotorIntake(1.0);
