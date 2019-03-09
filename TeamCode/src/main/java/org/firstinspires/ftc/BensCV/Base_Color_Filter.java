a93d75a238d2f454ff97dbad482d818cc7a77833
Navigator365 <40570437+Navigator365@users.noreply.github.com>
2019-03-09 10:05:42 -0500

    Commit to switch branch

--- TeamCode/src/main/java/org/firstinspires/ftc/BensCV/Base_Color_Filter.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.BensCV;
-
-import org.opencv.core.Mat;
-
-public abstract class Base_Color_Filter {
-    public abstract void process(Mat input, Mat mask);
-
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/BensCV/Base_Image_Triangulator.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.BensCV;
-
-import org.opencv.core.Mat;
-
-public abstract class Base_Image_Triangulator {
-    public abstract double calculateScore(Mat input);
-}
